b70e2e6a44b9beb8192bff35b7eec07a988010d8
Mihir Lad <mihirlad55@gmail.com>
2019-06-22 00:09:08 -0400

    Make symlink to ipc-bottom at start

--- scripts/spotify/launchlistener.sh
+++ scripts/spotify/launchlistener.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 
-/usr/bin/env python3 ~/scripts/spotify/py_spotify_listener.py
+/usr/bin/env python3 ~/install/polybar-spotify-controls/scripts/spotify/py_spotify_listener.py
+
+ln -sf /tmp/polybar_mqueue.$! /tmp/ipc-bottom
